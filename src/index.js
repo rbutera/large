520c08aedf0eaf777f27f7bcce1781b3fb262126
Rai Butera <rai@rbutera.com>
2018-08-13 16:23:08 +0100

    applied promise middleware to store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom"
 import React from "react"
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import { promiseMiddleware } from "./middleware"
 import App from "./App.js"
 import { Provider } from "react-redux"
 
@@ -9,7 +10,7 @@ const defaultState = { appName: "LARGE", articles: null }
 const reducer = function(state = defaultState, action) {
   return state
 }
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(promiseMiddleware))
 
 ReactDOM.render(
   <Provider store={store}>

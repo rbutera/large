0133123fff8c756f66902ddca53250ceabd9fc1c
Rai Butera <rai@rbutera.com>
2018-08-13 14:19:56 +0100

    updated to step 00

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from "react-dom"
+import React from "react"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+  render() {
+    return <h1>Hello, World!</h1>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))

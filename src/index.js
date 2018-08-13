a7c35b5b944d64d2185fa1707270ba372602f961
Rai Butera <rai@rbutera.com>
2018-08-13 18:43:08 +0100

    done with thinkster

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import React from "react"
 import App from "./App.js"
 import { Provider } from "react-redux"
 import store from "./store"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById("root")
 )
